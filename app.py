1a6fac0cc99df0c968cd36af77f31fa2214c52ee
Singuuu <jota.llopis@pm.me>
2023-03-13 13:52:26 +0100

    Update func name

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from satellite_czml import satellite_czml
 app = Flask(__name__)
 
 @app.post("/tle2czml")
-def create_store():
+def convert():
     request_data = request.get_json()
 
     tle = [[request_data["line0"], request_data["line1"], request_data["line2"]]]
