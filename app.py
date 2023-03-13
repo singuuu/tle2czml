59046febb5cbc89fc12a3b48aae257afba083080
Singuuu <jota.llopis@pm.me>
2023-03-13 14:16:50 +0100

    Add XML compatibility

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, request
 from satellite_czml import satellite_czml
+import xmltodict
 
 app = Flask(__name__)
 
 
-@app.post("/tle2czml")
-def convert():
+@app.post("/tle2czml/json")
+def convert_json():
+
     request_data = request.get_json()
 
-    tle = [[request_data["line0"], request_data["line1"], request_data["line2"]]]
+    tle = [[request_data["line0"],
+            request_data["line1"],
+            request_data["line2"]
+            ]]
+
+    return satellite_czml(tle_list=tle).get_czml(), 200
+
+
+@app.post("/tle2czml/xml")
+def convert_xml():
+
+    tle = [[xmltodict.parse(request.data)["tle"]["line0"],
+            xmltodict.parse(request.data)["tle"]["line1"],
+            xmltodict.parse(request.data)["tle"]["line2"]
+            ]]
 
     return satellite_czml(tle_list=tle).get_czml(), 200
