2457ae5d10e35112fb97b1dc1534ee1ad6146a8a
Singuuu <jota.llopis@pm.me>
2023-03-13 13:33:18 +0100

    Clean code and add Dockerfile

--- app.py
+++ app.py
@@ -6,10 +6,8 @@ app = Flask(__name__)
 @app.post("/tle2czml")
 def create_store():
     request_data = request.get_json()
-    tle0 = request_data["line0"]
-    tle1 = request_data["line1"]
-    tle2 = request_data["line2"]
-    tle = [[tle0, tle1, tle2]]
+
+    tle = [[request_data["line0"], request_data["line1"], request_data["line2"]]]
 
     return satellite_czml(tle_list=tle).get_czml(), 200
 
