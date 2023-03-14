beb62baaf06d2fb6948cc016e7c909b4c6a6bca8
Singuuu <jota.llopis@pm.me>
2023-03-14 18:06:34 +0100

    Add multiple TLE compatibility

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.post("/tle2czml/json")
 def convert_json():
-
     request_data = request.get_json()
 
     tle = [[request_data["line0"],
@@ -20,10 +19,32 @@ def convert_json():
 
 @app.post("/tle2czml/xml")
 def convert_xml():
-
     tle = [[xmltodict.parse(request.data)["tle"]["line0"],
             xmltodict.parse(request.data)["tle"]["line1"],
             xmltodict.parse(request.data)["tle"]["line2"]
             ]]
 
     return Response(satellite_czml(tle_list=tle).get_czml(), status=200, mimetype="application/json")
+
+
+@app.post("/tle2czml/multipleJson")
+def convert_test():
+    multiple_tle = []
+
+    data = request.get_json()
+
+    i = 0
+
+    for _ in data:
+        tle = []
+        element = [data[i]]
+
+        for line in element:
+            tle.append(line["line0"])
+            tle.append(line["line1"])
+            tle.append(line["line2"])
+
+        multiple_tle.append(tle)
+        i += 1
+
+    return Response(satellite_czml(tle_list=multiple_tle).get_czml(), status=200, mimetype="application/json")
